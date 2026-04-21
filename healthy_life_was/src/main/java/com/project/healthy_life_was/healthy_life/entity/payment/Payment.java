8a343c5820a1d43cd93871eb6d216cecc4655b9a
als5855 <wlals5855@naver.com>
2026-04-21 17:31:01 +0900

    payment-update

--- healthy_life_was/src/main/java/com/project/healthy_life_was/healthy_life/entity/payment/Payment.java
+++ healthy_life_was/src/main/java/com/project/healthy_life_was/healthy_life/entity/payment/Payment.java
@@ -1,4 +1,40 @@
 package com.project.healthy_life_was.healthy_life.entity.payment;
 
+import com.project.healthy_life_was.healthy_life.entity.order.Order;
+import com.project.healthy_life_was.healthy_life.entity.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id", nullable = false, updatable = false)
+    private Long paymentId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "payment_method", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Column(name = "payment_status", nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Column(name = "payment_date", nullable = false)
+    private LocalDateTime paymentDate;
+
 }
